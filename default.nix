5bded56306dc36c92cd5ea353ee5f8a2d6b89262
Someone Serge <newkozlukov@gmail.com>
2021-05-20 17:50:19 +0300

    try fixing Namespace Gtk not available error
    by adding glib and gobject-introspection (random stuff I saw people
    include on nixpkgs)

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
-{ gp-saml-gui, description, lib, buildPythonPackage, requests, pygobject }:
+{ gp-saml-gui, description, lib, buildPythonPackage, requests, pygobject
+, gobject-introspection, glib }:
 
 buildPythonPackage rec {
-    pname = "gp-saml-gui";
-    version = "0.1";
-    src = gp-saml-gui;
-    doCheck = false;
-    propagatedBuildInputs = [ requests pygobject ];
-    meta = with lib; {
-        inherit description;
-        homepage = "https://github.com/dlenski/gp-saml-gui";
-        license = licenses.gpl3Plus;
-    };
+  pname = "gp-saml-gui";
+  version = "0.1";
+  src = gp-saml-gui;
+  doCheck = false;
+  nativeBuildInputs = [ gobject-introspection ];
+  propagatedBuildInputs = [ requests pygobject glib ];
+  meta = with lib; {
+    inherit description;
+    homepage = "https://github.com/dlenski/gp-saml-gui";
+    license = licenses.gpl3Plus;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -1,27 +1,30 @@
 {
-  description = "gp-saml-gui: Interactively authenticate to GlobalProtect VPNs that require SAML";
+  description =
+    "gp-saml-gui: Interactively authenticate to GlobalProtect VPNs that require SAML";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.gp-saml-gui = {
-      url = "github:dlenski/gp-saml-gui";
-      flake = false;
+    url = "github:dlenski/gp-saml-gui";
+    flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, gp-saml-gui, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      let devshell = import inputs.devshell { inherit system; };
-          pkgs = import nixpkgs { inherit system; };
+      let
+        devshell = import inputs.devshell { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
       in rec {
-          devShell = import ./shell.nix { inherit devshell; };
-          packages.gp-saml-gui = (pkgs.callPackage ./default.nix {
-              inherit gp-saml-gui;
-              inherit (self) description;
-              inherit (pkgs.python3Packages) buildPythonPackage requests;
-              pygobject = pkgs.python3Packages.pygobject3;
-              });
-          defaultPackage = packages.gp-saml-gui;
-          apps.gp-saml-gui = flake-utils.lib.mkApp { drv = packages.gp-saml-gui; };
-          defaultApp = apps.gp-saml-gui;
+        devShell = import ./shell.nix { inherit devshell; };
+        packages.gp-saml-gui = (pkgs.callPackage ./default.nix {
+          inherit gp-saml-gui;
+          inherit (self) description;
+          inherit (pkgs.python3Packages) buildPythonPackage requests;
+          pygobject = pkgs.python3Packages.pygobject3;
+        });
+        defaultPackage = packages.gp-saml-gui;
+        apps.gp-saml-gui =
+          flake-utils.lib.mkApp { drv = packages.gp-saml-gui; };
+        defaultApp = apps.gp-saml-gui;
       });
 }
